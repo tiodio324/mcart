8411cf302b0dc49955464bf2d3ff488ceb05e739
tiodio324 <nikita324smirnovvv@gmail.com>
2025-03-21 23:22:46 +0300

    task 183903

--- kabinet-prodavtsa/.left.menu.php
+++ kabinet-prodavtsa/.left.menu.php
@@ -1,4 +1,11 @@
 <?
 $aMenuLinks = Array(
+	Array(
+		"Мои объявления", 
+		"/kabinet-prodavtsa/moi-obyavleniya/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
 );
 ?>
--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -22,25 +22,25 @@
               <h3 class="footer-heading mb-4">Navigations</h3>
             </div>
             <?$APPLICATION->IncludeComponent(
-	            "bitrix:menu", 
-	            "bottom_menu", 
-	            array(
-	            	"ALLOW_MULTI_SELECT" => "N",
-	            	"CHILD_MENU_TYPE" => "left",
-	            	"DELAY" => "N",
-	            	"MAX_LEVEL" => "1",
-	            	"MENU_CACHE_GET_VARS" => array(
-	            	),
-	            	"MENU_CACHE_TIME" => "3600",
-	            	"MENU_CACHE_TYPE" => "A",
-	            	"MENU_CACHE_USE_GROUPS" => "Y",
-	            	"MENU_THEME" => "site",
-	            	"ROOT_MENU_TYPE" => "left",
-	            	"USE_EXT" => "N",
-	            	"COMPONENT_TEMPLATE" => "bottom_menu"
-	            ),
-	            false
-            );?>
+	"bitrix:menu", 
+	"bottom_menu", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"MENU_THEME" => "site",
+		"ROOT_MENU_TYPE" => "",
+		"USE_EXT" => "N",
+		"COMPONENT_TEMPLATE" => "bottom_menu"
+	),
+	false
+);?>
           </div>
         </div>
           <?$APPLICATION->IncludeComponent(
